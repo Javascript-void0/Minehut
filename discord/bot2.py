901a886b6e21647326ca5880db22a56da2a48075
Java <indead1124@gmail.com>
2021-10-18 21:09:16 -0400

    discord messages

--- discord/bot2.py
+++ discord/bot2.py
@@ -51,6 +51,9 @@ async def on_message(message):
         msg = message.content
         name = msg.replace('```', '')
         await channel.edit(name=name)
+    if ".link" in message.content and message.channel.name != "link":
+        await message.delete()
+        await message.channel.send('```This command can only be used in #link```', delete_after=3)
     # if "Time" in message.content and message.channel.name == 'portal':
     #     channel = guild.get_channel(873434491123826689)
     #     time = timeFormat(message.content)
