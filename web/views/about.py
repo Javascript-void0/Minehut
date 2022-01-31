42544b8cfdfa4262bc89eb7c415c6a77d4bf3174
Java <indead1124@gmail.com>
2022-01-31 17:12:07 -0500

    start fill in info

--- web/views/about.py
+++ web/views/about.py
@@ -4,4 +4,4 @@ about = Blueprint('about', __name__)
 
 @about.route('/')
 def form():
-    return render_template('about/about.html', table_of_contents=1)
+    return render_template('about.html', table_of_contents=1)
--- web/views/wiki.py
+++ web/views/wiki.py
@@ -4,4 +4,4 @@ wiki = Blueprint('wiki', __name__)
 
 @wiki.route('/')
 def form():
-    return render_template('wiki/wiki.html', table_of_contents=0)
+    return render_template('wiki.html', table_of_contents=0)
