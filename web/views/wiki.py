c6ca7f9ec6bff3f11c7ce93157caa73649023ae8
Java <indead1124@gmail.com>
2022-02-04 14:22:16 -0500

    more web

--- web/views/wiki.py
+++ web/views/wiki.py
@@ -5,3 +5,7 @@ wiki = Blueprint('wiki', __name__)
 @wiki.route('/')
 def form():
     return render_template('wiki.html', table_of_contents=0)
+
+@wiki.route('/mob-drop-rates')
+def mob_drop_rates():
+    return render_template('mob-drop-rates.html', table_of_contents=1)
